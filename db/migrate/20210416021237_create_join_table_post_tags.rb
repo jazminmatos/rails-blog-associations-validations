7e77731403ab8e32669415fc8a664d0f252d8300
jazminmatos <jmatos1993@gmail.com>
2021-04-15 22:38:08 -0400

    Done.

--- db/migrate/20210416021237_create_join_table_post_tags.rb
+++ db/migrate/20210416021237_create_join_table_post_tags.rb
@@ -1,4 +1,4 @@
-class CreateJoinTablePostsTags < ActiveRecord::Migration[5.0]
+class CreateJoinTablePostTags < ActiveRecord::Migration[5.0]
   def change
     create_join_table :posts, :tags do |t|
       t.index [:post_id, :tag_id]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210415215122) do
+ActiveRecord::Schema.define(version: 20210416021237) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
